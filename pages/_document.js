f67010caa72304d2e40f7c5390d0628c6c7e01e6
Christopher Cole <cmc8066@psu.edu>
2025-04-15 14:39:26 -0400

    remove weird boarders

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/*include global fonts or meta tags here*/}
+        <style>{`
+          html, body {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            overflow-x: hidden;
+          }
+          *, *::before, *::after {
+            box-sizing: inherit;
+          }
+        `}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
